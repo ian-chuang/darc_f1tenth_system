392cc75026ad65ba0b8579283bb967322815bdb9
Ian Chuang <iantc104@gmail.com>
2024-05-08 19:43:16 -0700

    new map working on broken obs_detect

---
+++ f1tenth_stack/launch/autonomous_launch.py
@@ -0,0 +1,62 @@
+from http.server import executable
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from ament_index_python.packages import get_package_share_directory
+
+import os
+import yaml
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    obs_detect_config = os.path.join(
+        get_package_share_directory('f1tenth_stack'),
+        'config',
+        'obs_detect.yaml'
+    )
+    pure_pursuit_config = os.path.join(
+        get_package_share_directory('f1tenth_stack'),
+        'config',
+        'pure_pursuit.yaml'
+    )
+    gap_follow_config = os.path.join(
+        get_package_share_directory('f1tenth_stack'),
+        'config',
+        'gap_follow.yaml'
+    )
+
+    # Pure pursuit
+    pure_pursuit_node = Node(
+        package = 'pure_pursuit_pkg',
+        executable = 'pure_pursuit',
+        name = 'pure_pursuit',
+        parameters = [pure_pursuit_config]
+    )
+
+    # Obstacle detection
+    obs_detect_node = Node(
+        package = 'obs_detect_pkg',
+        executable = 'obs_detect_node',
+        name = 'obs_detect_node',
+        parameters = [obs_detect_config]
+    )
+
+    # Obstacle avoidance
+    gap_follow_node = Node(
+        package = 'gap_follow',
+        executable = 'reactive_node',
+        name = 'gap_follow',
+        parameters = [gap_follow_config]
+    )
+
+
+    ld.add_action(pure_pursuit_node)
+    ld.add_action(obs_detect_node)
+    ld.add_action(gap_follow_node)
+
+
+
+    return ld
